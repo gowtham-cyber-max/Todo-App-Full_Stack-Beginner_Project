bddc4831128e9d1949d605a71a4fab6ce2710f29
gowtham-cyber-max <gowtham4262472@gmail.com>
2024-03-27 15:16:30 +0530

    err

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App1 from './chat/App1';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App1/>
+    <App/>
   </React.StrictMode>
 );
 
