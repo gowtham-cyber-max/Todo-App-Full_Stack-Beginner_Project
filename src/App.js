2f92b7ca94f2814bcdba512fa22c766cf5613b37
gowtham-cyber-max <gowtham4262472@gmail.com>
2024-04-08 17:30:04 +0530

    dependies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { HashRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
 import Todo from './Todo/Todo';
+import Login from './Todo/Login';
 
 
 
 function App() {
   return (
     <div className="App">
-    <Todo/>
+      
+      
+        <Route path='/' element={<Login/>}/>
+
+        <Route path='/Todo' element={<Todo/>}/>
+    
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App/>
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
